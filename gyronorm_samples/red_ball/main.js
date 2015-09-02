c702e9d28af6d210766dbe0a0c7a25485e4c2426
Doruk Eker <doruk@dorukeker.com>
2015-09-02 09:50:35 +0200

    Changes in the js file

--- gyronorm_samples/red_ball/main.js
+++ gyronorm_samples/red_ball/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function(){
   // Define variables for later use
-  var ballCurrentX, ballCurrentY, ballNewX, ballNewY, containerWidth, containerHeight;
+  var ballCurrentX, ballCurrentY, ballNewX, ballNewY, containerWidth, containerHeight, ballWidth, ballHeight;
 
+  // Set initial width and hieght for the container and the ball
   containerWidth = $('#container').width();
   containerHeight = $('#container').height();
 
-  // Listen for any change in the orientation
-  window.addEventListener("orientationchange", onOrientationChangeHandler, false);
+  ballWidth = $('#ball').width();
+  ballHeight = $('#ball').height();
 
   // Move the ball to the center of the screen
   var ballInitialX = (containerWidth - $('#ball').width()) / 2;
@@ -24,20 +25,20 @@ $(document).ready(function(){
     // Catch if the DeviceOrientation or DeviceMotion is not supported by the browser or device
     console.log(e);
   });
+
+  // Callback function for gn.start();
+  var onGyronormCallback = function(data){
+
+    ballCurrentX = parseInt($('#ball').css('left'));
+    ballNewX = Math.max(ballCurrentX-(data.dm.gx * 5),0);
+    ballNewX = Math.min(ballNewX,containerWidth - ballWidth);
+    $('#ball').css('left',ballNewX);
+
+    ballCurrentY = parseInt($('#ball').css('top'));
+    ballNewY = Math.max(ballCurrentY+(data.dm.gy * 5),0);
+    ballNewY = Math.min(ballNewY,containerHeight - ballHeight);
+    $('#ball').css('top',ballNewY);
+
+  }
+
 });
-
-// Callback function for gn.start();
-var onGyronormCallback = function(data){
-  ballCurrentX = parseInt($('#ball').css('left'));
-  ballNewX = Math.max(ballCurrentX-(data.dm.gx * 5),0);
-  ballNewX = Math.min(ballNewX,containerWidth);
-  $('#ball').css('left',ballNewX);
-    // data.dm.gx       ( devicemotion event accelerationIncludingGravity x value )
-    // data.dm.gy       ( devicemotion event accelerationIncludingGravity y value )
-    // data.dm.gz       ( devicemotion event accelerationIncludingGravity z value )
-
-}
-
-var onOrientationChangeHandler = function(){
-
-}
